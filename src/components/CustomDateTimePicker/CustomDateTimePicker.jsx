4f5def2cac698c7be11d04a3db7870b00231cb04
AndreMashukov <andre.mashukov@gmail.com>
2024-03-26 20:15:03 +0800

    changes

--- src/components/CustomDateTimePicker/CustomDateTimePicker.jsx
+++ src/components/CustomDateTimePicker/CustomDateTimePicker.jsx
@@ -1,6 +1,11 @@
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import TextField from '@mui/material/TextField';
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import IconButton from '@mui/material/IconButton';
 import moment from 'moment';
 import { useState } from 'react';
+
 
 export const CustomDateTimePicker = () => {
   const [value, setValue] = useState(moment("2022-04-17T15:30"))
@@ -9,6 +14,8 @@ export const CustomDateTimePicker = () => {
       label="Datetime picker"
       value={value}
       onChange={(newValue) => setValue(newValue)}
+      format="DD/MM/YYYY HH:mm"
+      // slotProps={{ textField: { InputProps: { endAdornment: <AccessTimeIcon /> } } }}
     />
   )
 }
