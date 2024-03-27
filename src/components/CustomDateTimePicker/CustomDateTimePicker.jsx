53592ebb119968b3c0c2edb2113908a9afc6ba6c
AndreMashukov <andre.mashukov@gmail.com>
2024-03-27 09:08:41 +0800

    changes

--- src/components/CustomDateTimePicker/CustomDateTimePicker.jsx
+++ src/components/CustomDateTimePicker/CustomDateTimePicker.jsx
@@ -1,21 +1,46 @@
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import TextField from '@mui/material/TextField';
-import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import IconButton from '@mui/material/IconButton';
 import moment from 'moment';
 import { useState } from 'react';
 
-
 export const CustomDateTimePicker = () => {
-  const [value, setValue] = useState(moment("2022-04-17T15:30"))
+  const [value, setValue] = useState(moment("2022-04-17T15:30"));
+  const [error, setError] = useState(false);
+  const [helperText, setHelperText] = useState('');
+
+  const handleDateChange = (newValue) => {
+    setValue(newValue);
+    
+    console.log(newValue.format());
+    // console.log(newValue.isAfter(moment.now());
+
+    // Add your validation logic here
+    if (newValue.isBefore(moment.now())) {
+      setError(true);
+      setHelperText('Error: Invalid date');
+    } else {
+      setError(false);
+      setHelperText('');
+    }
+  };
+
   return (
     <DateTimePicker
       label="Datetime picker"
       value={value}
-      onChange={(newValue) => setValue(newValue)}
+      onChange={handleDateChange}
       format="DD/MM/YYYY HH:mm"
-      // slotProps={{ textField: { InputProps: { endAdornment: <AccessTimeIcon /> } } }}
+      slotProps={{
+        textField: {
+          // InputProps: {
+          //   startAdornment: <AccessTimeIcon />,
+          // },
+          error: error,
+          helperText: helperText,
+        },
+      }}
     />
   )
 }
