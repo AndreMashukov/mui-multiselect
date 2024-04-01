302071634876a5d05f7ae660ca05006ae4a13aca
Andrey Mashukov <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>
2024-04-01 13:42:12 +0800

    changes

--- src/views/ModalContainerTestView.jsx
+++ src/views/ModalContainerTestView.jsx
@@ -1,47 +1,47 @@
-import { Button, Grid } from "@mui/material"
-import { useState } from "react"
-import ModalContainer from "../components/ModalContainer/ModalContainer";
-import { useGenericReducer } from "../hooks/useGenericReducer";
+import { Button, Grid } from '@mui/material'
+import ModalContainer from '../components/ModalContainer/ModalContainer'
+import { useGenericReducer } from '../hooks/useGenericReducer'
 
 export const ModalContainerTestView = () => {
   // const [open, setOpen] = useState(false)
 
-  const {state, actions} = useGenericReducer({
+  const { state, actions } = useGenericReducer({
     reducers: {
       setOpen: (state, action) => ({
         ...state,
-        open: action.payload,
+        open: action.payload
       }),
       setTitle: (state, action) => ({
         ...state,
-        title: action.payload,
-      }),
+        title: action.payload
+      })
     },
     initialState: {
       open: false,
-      title: "",
+      title: ''
     }
   })
   return (
     <>
-      <Button onClick={() => {
-        actions.setTitle("Title");
-        actions.setOpen(true);
-      }}>
+      <Button
+        onClick={() => {
+          actions.setTitle('Title')
+          actions.setOpen(true)
+        }}
+      >
         Open
       </Button>
       {state.open && (
         <ModalContainer
-          open={state.open}s
+          open={state.open}
+          s
           setOpen={actions.setOpen}
           title={state.title}
         >
-          <Grid container sx={{ width: "300px" }}>
+          <Grid container sx={{ width: '300px' }}>
             Text
           </Grid>
-          <Grid container>
-            Text
-          </Grid>
+          <Grid container>Text</Grid>
         </ModalContainer>
       )}
     </>
