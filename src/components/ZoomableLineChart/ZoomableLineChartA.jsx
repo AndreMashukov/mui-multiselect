8ac87108e26d930fcf93eff95dbe8dbc8d222bf3
Andrey Mashukov <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>
2024-04-24 15:35:33 +0800

    minor changes

--- src/components/ZoomableLineChart/ZoomableLineChartA.jsx
+++ src/components/ZoomableLineChart/ZoomableLineChartA.jsx
@@ -9,11 +9,6 @@ const ZoomableLineChart = () => {
   const margin = { top: 10, right: 30, bottom: 30, left: 60 };
   const width = WIDTH - margin.left - margin.right;
   const height = HEIGHT - margin.top - margin.bottom;
-  let idleTimeout;
-
-  function idled() {
-    idleTimeout = null;
-  }
 
   const createSvg = (ref) => {
     const svg = d3
@@ -72,96 +67,6 @@ const ZoomableLineChart = () => {
       .attr("y", 0);
   };
 
-  const handleChartDoubleClick = (data, x, y, xAxis, line) => {
-    x.domain(
-      d3.extent(data, function (d) {
-        return d.date;
-      })
-    );
-    xAxis.transition().call(d3.axisBottom(x));
-    line
-      .select(".line")
-      .transition()
-      .attr(
-        "d",
-        d3
-          .line()
-          .x(function (d) {
-            return x(d.date);
-          })
-          .y(function (d) {
-            return y(d.value);
-          })
-      );
-  };
-
-  const handleBrushEnd = (event, d, x, y, svg, brush) => {
-    const extent = event.selection;
-
-    if (!extent) {
-      if (!idleTimeout) return (idleTimeout = setTimeout(idled, 350));
-      x.domain([4, 8]);
-    } else {
-      x.domain([x.invert(extent[0]), x.invert(extent[1])]);
-      svg.select(".brush").call(brush.move, null);
-    }
-
-    svg.select(".x-axis").transition().duration(1000).call(d3.axisBottom(x));
-    svg
-      .select(".line")
-      .transition()
-      .duration(1000)
-      .attr(
-        "d",
-        d3
-          .line()
-          .x(function (d) {
-            return x(d.date);
-          })
-          .y(function (d) {
-            return y(d.value);
-          })
-      );
-  };
-
-  const createBrush = (x, y, svg, line) => {
-    const brush = d3
-      .brushX()
-      .extent([
-        [0, 0],
-        [width, height],
-      ])
-      .on("end", (e, d) => handleBrushEnd(e, d, x, y, svg, brush));
-
-    line.append("g").attr("class", "brush").call(brush);
-  };
-
-  const createLine = (svg, data, x, y, xAxis) => {
-    const line = svg.append("g").attr("clip-path", "url(#clip)");
-
-    line
-      .append("path")
-      .datum(data)
-      .attr("class", "line")
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-width", 1.5)
-      .attr(
-        "d",
-        d3
-          .line()
-          .x(function (d) {
-            return x(d.date);
-          })
-          .y(function (d) {
-            return y(d.value);
-          })
-      );
-
-    createBrush(x, y, svg, line);
-    svg.on("dblclick", () => handleChartDoubleClick(data, x, y, xAxis, line));
-  };
-
   useEffect(() => {
     d3.select(ref.current).selectAll("*").remove();
 
@@ -175,7 +80,94 @@ const ZoomableLineChart = () => {
     ).then(function (data) {
       const { x, y, xAxis } = createAxes(svg, data);
       addClipping(svg);
-      createLine(svg, data, x, y, xAxis);
+
+      const brush = d3
+        .brushX()
+        .extent([
+          [0, 0],
+          [width, height],
+        ])
+        .on("end", updateChart);
+
+      const line = svg.append("g").attr("clip-path", "url(#clip)");
+
+      line
+        .append("path")
+        .datum(data)
+        .attr("class", "line")
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr(
+          "d",
+          d3
+            .line()
+            .x(function (d) {
+              return x(d.date);
+            })
+            .y(function (d) {
+              return y(d.value);
+            })
+        );
+
+      line.append("g").attr("class", "brush").call(brush);
+
+      let idleTimeout;
+      function idled() {
+        idleTimeout = null;
+      }
+
+      function updateChart(event, d) {
+        const extent = event.selection;
+
+        if (!extent) {
+          if (!idleTimeout) return (idleTimeout = setTimeout(idled, 350));
+          x.domain([4, 8]);
+        } else {
+          x.domain([x.invert(extent[0]), x.invert(extent[1])]);
+          line.select(".brush").call(brush.move, null);
+        }
+
+        xAxis.transition().duration(1000).call(d3.axisBottom(x));
+        line
+          .select(".line")
+          .transition()
+          .duration(1000)
+          .attr(
+            "d",
+            d3
+              .line()
+              .x(function (d) {
+                return x(d.date);
+              })
+              .y(function (d) {
+                return y(d.value);
+              })
+          );
+      }
+
+      svg.on("dblclick", function () {
+        x.domain(
+          d3.extent(data, function (d) {
+            return d.date;
+          })
+        );
+        xAxis.transition().call(d3.axisBottom(x));
+        line
+          .select(".line")
+          .transition()
+          .attr(
+            "d",
+            d3
+              .line()
+              .x(function (d) {
+                return x(d.date);
+              })
+              .y(function (d) {
+                return y(d.value);
+              })
+          );
+      });
     });
   }, []);
 
