49d0c1e411aad785ed3254fbe180efb433a2f87d
Andrey Mashukov <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>
2024-04-25 10:06:17 +0800

    changes

--- src/views/ZoomableLineChartView.jsx
+++ src/views/ZoomableLineChartView.jsx
@@ -1,19 +1,35 @@
 import { useState } from "react";
-import ZoomableLineChart from "../components/ZoomableLineChart/ZoomableLineChartA";
+import ZoomableLineChartA from "../components/ZoomableLineChart/ZoomableLineChartA";
+import ZoomableLineChart from "../components/ZoomableLineChart/ZoomableLineChart";
+import SelectorSecondary from "../components/SelectorSecondary/SelectorSecondary";
+import { Grid } from "@mui/material";
 
 const ZoomableLineChartView = () => {
-  // const [data, setData] = useState(
-  //   Array.from({ length: 50 }, () => Math.round(Math.random() * 100))
-  // );
+  const [data, setData] = useState(
+    Array.from({ length: 50 }, () => Math.round(Math.random() * 100))
+  );
+  const [selectedOption, setSelectedOption] = useState();
+
+  const selectorOptions = [
+    { id: "day", label: "1 day" },
+    { id: "week", label: "1 week" },
+    { id: "month", label: "1 month" },
+    { id: "year", label: "1 year" },
+  ];
 
   return (
     <div style={{ padding: "20px" }}>
-      <ZoomableLineChart />
-      {/* <button
-        onClick={() => setData([...data, Math.round(Math.random() * 100)])}
-      >
-        Add data
-      </button> */}
+      <Grid container justifyContent="flex-end">
+        <SelectorSecondary
+          options={selectorOptions}
+          setSelectedOption={setSelectedOption}
+          selectedOption={selectedOption}
+          extraStyles={{ mb: 2 }}
+        />
+      </Grid>
+      <div>
+        <ZoomableLineChartA />
+      </div>
     </div>
   );
 };
