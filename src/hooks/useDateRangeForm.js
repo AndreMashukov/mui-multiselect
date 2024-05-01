b31c6f4b51abcc7afd767ae76f6186107d635143
Andrey Mashukov <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>
2024-05-01 09:04:39 +0800

    changes

---
+++ src/hooks/useDateRangeForm.js
@@ -0,0 +1,40 @@
+import { useFormik } from "formik";
+import moment from "moment";
+import * as yup from "yup";
+
+export const useDataRangeForm = () => {
+  const validationSchema = yup.object({
+    startDate: yup
+      .string()
+      .required()
+      .test("valid", "Start date is invalid", (value) => {
+        return moment(value).isValid();
+      }),
+    endDate: yup
+      .string()
+      .required()
+      .test("valid", "End date is invalid", (value) => {
+        return moment(value).isValid();
+      })
+      .test("after", "End date must be after start date", function (value) {
+        const { startDate } = this.parent;
+        return moment(value).isAfter(moment(startDate));
+      }),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      startDate: undefined,
+      endDate: undefined,
+    },
+    validationSchema,
+    onSubmit: (values) => {
+      console.log("submit", {
+        startDate: values.startDate,
+        endDate: values.endDate,
+      });
+    },
+  });
+
+  return formik;
+}
--- src/views/RangePickerView.jsx
+++ src/views/RangePickerView.jsx
@@ -1,44 +1,11 @@
-import { useFormik } from "formik";
-import moment from "moment";
 import { Button } from "@mui/material";
-import * as yup from "yup";
 import RangePickerWithSelector from "../components/RangePickerWithSelector/RangePickerWithSelector";
+import { useDataRangeForm } from "../hooks/useDateRangeForm";
 
 // https://mui.com/x/react-date-pickers/date-time-picker/
 
 export const RangePickerView = () => {
-  const validationSchema = yup.object({
-    startDate: yup
-      .string()
-      .required()
-      .test("valid", "Start date is invalid", (value) => {
-        return moment(value).isValid();
-      }),
-    endDate: yup
-      .string()
-      .required()
-      .test("valid", "End date is invalid", (value) => {
-        return moment(value).isValid();
-      })
-      .test("after", "End date must be after start date", function (value) {
-        const { startDate } = this.parent;
-        return moment(value).isAfter(moment(startDate));
-      }),
-  });
-
-  const formik = useFormik({
-    initialValues: {
-      startDate: undefined,
-      endDate: undefined,
-    },
-    validationSchema,
-    onSubmit: (values) => {
-      console.log("submit", {
-        startDate: values.startDate,
-        endDate: values.endDate,
-      });
-    },
-  });
+  const formik = useDataRangeForm();
 
   return (
     <>
