257040ed5f246cd65922cdae14fed398f7691a88
Andrey Mashukov <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>
2024-05-01 10:29:54 +0800

    changes

--- src/components/ZoomableLineChart/ZoomableLineChart.jsx
+++ src/components/ZoomableLineChart/ZoomableLineChart.jsx
@@ -81,7 +81,16 @@ const ZoomableLineChart = ({ data, width, height }) => {
   const handleMoveCursor = (event, data, x, y, focus, focusText) => {
     const [x0] = d3.pointer(event);
     const bisect = d3.bisector((d) => d.date).left;
-    const i = bisect(data, x.invert(x0), 1);
+    // Get the index of the data point that the mouse is currently over
+    let i = bisect(data, x.invert(x0)); // Remove the `1` argumentw
+
+    // If the index is 0, check if the mouse is closer to the first point or the second point
+    if (i === 0) {
+      const d0 = data[0];
+      const d1 = data[1];
+      i = x.invert(x0) - d0.date > d1.date - x.invert(x0) ? 1 : 0;
+    }
+
     const selectedData = data[i];
     focus.attr("cx", x(selectedData.date)).attr("cy", y(selectedData.value));
     focusText
