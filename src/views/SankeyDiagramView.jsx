5157bee46ee0fa32c719ac5a05d8abf3d13c1d93
Andrey Mashukov <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>
2024-05-08 16:29:54 +0800

    minor changes

--- src/views/SankeyDiagramView.jsx
+++ src/views/SankeyDiagramView.jsx
@@ -8,6 +8,11 @@ const sankeyData = {
     { id: 2, name: "node2" },
     { id: 3, name: "node3" },
     { id: 4, name: "node4" },
+    { id: 5, name: "node5" },
+    { id: 6, name: "node6" },
+    { id: 7, name: "node7" },
+    { id: 8, name: "node8" },
+    { id: 9, name: "node9" },
   ],
   links: [
     { source: 0, target: 2, value: 2 },
@@ -17,9 +22,13 @@ const sankeyData = {
     { source: 2, target: 3, value: 2 },
     { source: 2, target: 4, value: 2 },
     { source: 3, target: 4, value: 4 },
+    { source: 5, target: 6, value: 3 },
+    { source: 5, target: 7, value: 3 },
+    { source: 6, target: 8, value: 3 },
+    { source: 7, target: 8, value: 3 },
+    { source: 8, target: 9, value: 6 },
   ],
 };
-
 export const SankeyDiagramView = () => {
   return (
     <div style={{ padding: "20px", width: 800 }}>
@@ -27,4 +36,4 @@ export const SankeyDiagramView = () => {
       <SankeyDiagram sankeyData={sankeyData} />
     </div>
   );
-}
+};
