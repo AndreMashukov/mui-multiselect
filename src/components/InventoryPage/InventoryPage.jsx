2c8de98c2bfc5bbef3e2aaf29dfff5985849fdd9
Andrey Mashukov <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>
2024-05-21 19:08:23 +0800

    changes

--- src/components/InventoryPage/InventoryPage.jsx
+++ src/components/InventoryPage/InventoryPage.jsx
@@ -1,5 +1,5 @@
 import { useTranslation } from "react-i18next";
-import { useContext } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import DataTable from "react-data-table-component";
 import Grid from "@mui/material/Grid";
 import { useTableDefinitions } from "./hooks/useTableDefinitions.jsx";
@@ -10,20 +10,22 @@ import styled from "styled-components";
 
 const StyledDataTable = styled(DataTable)`
   .rdt_TableHead {
-    position: fixed;
+    position: ${(props) => (props.stickyHeader ? "fixed" : "static")};
     top: 0;
-    z-index: 100;
+    z-index: 1;
     background: white;
   }
 `;
 
 const Wrapper = styled.div`
-  height: 100vh; 
+  height: 100vh;
   overflow: auto;
 `;
 
 const InventoryPage = () => {
   const { t } = useTranslation();
+  const wrapperRef = useRef(null);
+  const tableRef = useRef(null);
   const { state, actions } = useContext(InventoryPageContext);
 
   const { getTableColums } = useTableDefinitions({
@@ -42,6 +44,30 @@ const InventoryPage = () => {
     </>
   );
 
+  const [stickyHeader, setStickyHeader] = useState();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (tableRef.current) {
+        const tableTopPosition = tableRef.current.getBoundingClientRect().top;
+        const header = tableRef.current.querySelector(".rdt_TableHead");
+        const headerHeight = header.getBoundingClientRect().height;
+        const isHeaderOutOfViewport = tableTopPosition < -headerHeight;
+        setStickyHeader(isHeaderOutOfViewport);
+      }
+    };
+
+    if (wrapperRef.current) {
+      wrapperRef.current.addEventListener("scroll", handleScroll);
+    }
+
+    return () => {
+      if (wrapperRef.current) {
+        wrapperRef.current.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, []);
+
   return (
     <>
       {/* <TicketMenu
@@ -53,36 +79,39 @@ const InventoryPage = () => {
       /> */}
       <Grid container spacing={2}>
         <Grid item xs={12}>
-          <Wrapper>
-            <StyledDataTable
-              columns={tableColumns}
-              data={state.tableRows}
-              progressPending={state.loading}
-              pagination
-              paginationServer
-              paginationTotalRows={state.totalRows}
-              onChangePage={actions.setPage}
-              onChangeRowsPerPage={actions.setItemsPerPage}
-              onColumnOrderChange={() => {}}
-              onSort={actions.setSort}
-              striped={true}
-              highlightOnHover={true}
-              enableColumnResizing
-              expandableRows
-              responsive={true}
-              pointerOnHover={true}
-              paginationRowsPerPageOptions={[10, 20, 50, 100, 200]}
-              expandableRowsComponent={InventoryDetail}
-              defaultColumn={{
-                maxSize: 400,
-                minSize: 80,
-                size: 150,
-              }}
-              subHeader
-              subHeaderComponent={subHeaderComponent}
-              subHeaderAlign="left"
-              subHeaderWrap
-            />
+          <Wrapper ref={wrapperRef}>
+            <div ref={tableRef}>
+              <StyledDataTable
+                stickyHeader={stickyHeader}
+                columns={tableColumns}
+                data={state.tableRows}
+                progressPending={state.loading}
+                pagination
+                paginationServer
+                paginationTotalRows={state.totalRows}
+                onChangePage={actions.setPage}
+                onChangeRowsPerPage={actions.setItemsPerPage}
+                onColumnOrderChange={() => {}}
+                onSort={actions.setSort}
+                striped={true}
+                highlightOnHover={true}
+                enableColumnResizing
+                expandableRows
+                responsive={true}
+                pointerOnHover={true}
+                paginationRowsPerPageOptions={[10, 20, 50, 100, 200]}
+                expandableRowsComponent={InventoryDetail}
+                defaultColumn={{
+                  maxSize: 400,
+                  minSize: 80,
+                  size: 150,
+                }}
+                subHeader
+                subHeaderComponent={subHeaderComponent}
+                subHeaderAlign="left"
+                subHeaderWrap
+              />
+            </div>
           </Wrapper>
         </Grid>
       </Grid>
