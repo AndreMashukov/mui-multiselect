1da2e4be307c302966ecf0e8b55a6474fcb045e7
Andrey Mashukov <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>
2024-06-11 19:06:59 +0800

    changes

--- src/components/AnimatedList/AnimatedList.jsx
+++ src/components/AnimatedList/AnimatedList.jsx
@@ -2,17 +2,26 @@ import { useState } from "react";
 import {
   Box,
   Button,
+  Divider,
   List,
   Stack,
-  ListItem,
   Typography,
   useTheme,
 } from "@mui/material";
-import { Shuffle } from "@mui/icons-material";
+import RefreshIcon from "@mui/icons-material/Refresh";
 import { COLUMN_WIDTH, INTERNET_TRAFFIC_LEADER_BOARD } from "./constants";
 import { motion, AnimatePresence } from "framer-motion";
 import ListItemComponent from "./ListItemComponent/ListItemComnponent";
 import moment from "moment";
+
+const VerticalDivider = () => (
+  <Box display="flex" alignItems="center" sx={{ mx: 2 }}>
+    <Divider
+      orientation="vertical"
+      sx={{ bgcolor: "grey.100", height: "25px", width: "2px" }}
+    />
+  </Box>
+);
 
 const AnimatedList = () => {
   const [items, setItems] = useState(INTERNET_TRAFFIC_LEADER_BOARD);
@@ -57,9 +66,9 @@ const AnimatedList = () => {
       sx={{
         minWidth: "500px",
         width: {
-          xs: "90vw", // 90% of viewport width for small devices
-          sm: "70vw", // 70% of viewport width for medium devices
-          md: "50vw", // 50% of viewport width for large devices
+          xs: "90vw",
+          sm: "70vw",
+          md: "70vw",
         },
         [theme.breakpoints.down("sm")]: {
           minWidth: "300px", // Minimum width of 300px for small devices
@@ -78,13 +87,24 @@ const AnimatedList = () => {
             Last Updated: {lastUpdated.format("MMM DD, YYYY hh:mm:ss")}
           </Typography>
         </Stack>
-        <Button
-          startIcon={<Shuffle />}
-          variant="contained"
-          onClick={shuffleItems}
+        <Stack
+          spacing={2}
+          justifyContent="space-between"
+          direction="row"
+          alignItems="flex-end"
         >
-          Shuffle
-        </Button>
+          <Button
+            startIcon={<RefreshIcon />}
+            variant="contained"
+            onClick={shuffleItems}
+          >
+            Refresh
+          </Button>
+          <VerticalDivider />
+          <Typography>Last 24 hours Peak</Typography>
+          <VerticalDivider />
+          <Typography>Last 7 days Peak</Typography>
+        </Stack>
       </Stack>
       <List sx={{ position: "relative" }}>
         {/* Fixed Header Row */}
