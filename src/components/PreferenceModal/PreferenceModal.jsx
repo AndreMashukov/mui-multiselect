a233ae23294a854540b053f9b16146cc468065ff
Andrey Mashukov <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>
2024-06-27 19:10:27 +0800

    changes

--- src/components/PreferenceModal/PreferenceModal.jsx
+++ src/components/PreferenceModal/PreferenceModal.jsx
@@ -5,6 +5,7 @@ import {
   DialogContent,
   Divider,
   Stack,
+  Typography,
 } from "@mui/material";
 import ModalHeader from "../Modal/ModalHeader/ModalHeader";
 import { StyledDialog } from "../Modal/StyledDialog.styled";
@@ -21,9 +22,35 @@ const PreferenceModal = ({ showHideColumnsModal, setShowHideColumnsModal }) => {
       />
 
       <DialogContent>
-        <Stack>Top</Stack>
+        <Stack sx={{ mb: 2 }}>
+          <Stack justifyContent="flex-start">
+            <Typography
+              variant="h6"
+              sx={{
+                fontWeight: "bold",
+              }}
+            >
+              Default Country and Company
+            </Typography>
+          </Stack>
+          <Stack sx={{ ml: 5 }}>
+            <Typography variant="body1">Country: United States</Typography>
+            <Typography variant="body1">Company: Apple</Typography>
+          </Stack>
+        </Stack>
         <Divider />
-        <Stack>Bottom</Stack>
+        <Stack sx={{ mt: 2 }}>
+          <Stack justifyContent="flex-start">
+            <Typography
+              variant="h6"
+              sx={{
+                fontWeight: "bold",
+              }}
+            >
+              Current Layout
+            </Typography>
+          </Stack>
+        </Stack>
       </DialogContent>
       <DialogActions sx={{ position: "relative" }}>
         <Box justifyContent="flex-end">
